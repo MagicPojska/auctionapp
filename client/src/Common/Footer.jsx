61287d425469a47173befcf514566fd58fdfd7ae
mrrobot <pojskicsafet@gmail.com>
2022-02-17 12:54:59 +0100

    Changed font color and added hover effect

--- client/src/Common/Footer.jsx
+++ client/src/Common/Footer.jsx
@@ -6,22 +6,28 @@ const Footer = () => {
     <footer className="h-96 bg-[#252525] w-full text-white flex flex-col md:flex-row justify-between py-8 px-10 sm:px-48 md:px-40 mt-20">
       <div className="">
         <h5 className="pb-6 text-gray-400">AUCTION</h5>
-        <ul className="space-y-[12px] text-base">
+        <ul className="space-y-[12px] text-base text-[#D8D8D8] ">
           <li>
-            <Link to="/about">About Us</Link>
+            <Link to="/about" className="hover:text-white">
+              About Us
+            </Link>
           </li>
           <li>
-            <Link to="/terms-and-conditions">Terms and Conditions</Link>
+            <Link to="/terms-and-conditions" className="hover:text-white">
+              Terms and Conditions
+            </Link>
           </li>
           <li>
-            <Link to="/privacy-and-policy">Privacy and Policy</Link>
+            <Link to="/privacy-and-policy" className="hover:text-white">
+              Privacy and Policy
+            </Link>
           </li>
         </ul>
       </div>
 
       <div className="">
         <h5 className="pb-6 text-gray-400">GET IN TOUCH</h5>
-        <ul className="space-y-[12px]">
+        <ul className="space-y-[12px] text-[#D8D8D8]">
           <li>
             <span>Call Us at +123 797-567-2535</span>
           </li>
