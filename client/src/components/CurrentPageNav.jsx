cdac9e3c86e24b064f04e9f22c40da7c554b537e
MagicPojska <pojskicsafet@gmail.com>
2022-02-22 09:39:15 +0100

    Made CurrentPageNav fully dynamic

--- client/src/components/CurrentPageNav.jsx
+++ client/src/components/CurrentPageNav.jsx
@@ -1,7 +1,7 @@
 const CurrentPageNav = ({ props }) => {
   return (
     <div className="h-[60px] bg-[#FCFCFC] flex justify-between items-center px-[160px] font-normal text-base">
-      <span>Terms and Conditions</span>
+      <span>{props}</span>
       <div className="font-normal text-base flex text-[#9B9B9B]">
         <span>Home &nbsp;</span> <icon className>&#10132; &nbsp;</icon>{" "}
         <span className="text-violet-400">{props}</span>
