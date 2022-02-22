f959810b1edb6d0e0f5d1d90c20329b8fcb16e15
MagicPojska <pojskicsafet@gmail.com>
2022-02-22 10:27:58 +0100

    Changed text and added new photos

--- client/src/Pages/AboutPage.jsx
+++ client/src/Pages/AboutPage.jsx
@@ -4,10 +4,10 @@ const AboutPage = () => {
   return (
     <>
       <CurrentPageNav props={"About Us"} />
-      <div className="mx-10 lg:mx-40 2xl:mx-60 flex flex-col md:flex-row mt-16">
+      <div className="mx-10 lg:mx-40 2xl:mx-60 flex flex-col md:flex-row justify-center mt-16">
         <div className="md:basis-1/2 text-base font-light leading-relaxed text-gray-400 mr-[22px]">
           <h1 className="font-normal leading-tight text-4xl text-black mb-8">
-            Privacy Policy
+            About Us
           </h1>
           <p className="mb-8">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
@@ -54,12 +54,12 @@ const AboutPage = () => {
           />
           <div className="flex space-x-5 mt-6">
             <img
-              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              src="https://images.unsplash.com/photo-1438109491414-7198515b166b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
               className="object-contain w-full"
               alt="woman"
             />
             <img
-              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              src="https://images.unsplash.com/photo-1517677129300-07b130802f46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
               className="object-contain w-full"
               alt="woman"
             />
