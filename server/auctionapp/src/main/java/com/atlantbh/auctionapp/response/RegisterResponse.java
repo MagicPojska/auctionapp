32ef6ca8916c9a5bed83791de49383b99ae9931d
MagicPojska <pojskicsafet@gmail.com>
2022-02-23 13:59:51 +0100

    Implemented register response class

--- server/auctionapp/src/main/java/com/atlantbh/auctionapp/response/RegisterResponse.java
+++ server/auctionapp/src/main/java/com/atlantbh/auctionapp/response/RegisterResponse.java
@@ -1,5 +1,19 @@
 package com.atlantbh.auctionapp.response;
 
-public class RegisterResponse {
+import com.atlantbh.auctionapp.model.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@Setter
+@Getter
+public class RegisterResponse {
+    private User user;
+    private String token;
+
+    public RegisterResponse(User user, String token) {
+        this.user = user;
+        this.token = token;
+    }
 }
