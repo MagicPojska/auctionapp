0d61d80fff09920feef2be0e809b477d37acc851
MagicPojska <pojskicsafet@gmail.com>
2022-02-23 11:42:06 +0100

    Added @Configuration annotation in SecurityConfig

--- server/auctionapp/src/main/java/com/atlantbh/auctionapp/config/SecurityConfig.java
+++ server/auctionapp/src/main/java/com/atlantbh/auctionapp/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.atlantbh.auctionapp.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
