8c580992d1cbf43ceb6d4950111553ca6e87d7ee
MagicPojska <pojskicsafet@gmail.com>
2022-02-23 13:24:42 +0100

    Added register service in UserService

--- server/auctionapp/src/main/java/com/atlantbh/auctionapp/service/UserService.java
+++ server/auctionapp/src/main/java/com/atlantbh/auctionapp/service/UserService.java
@@ -2,6 +2,7 @@ package com.atlantbh.auctionapp.service;
 
 import com.atlantbh.auctionapp.model.User;
 import com.atlantbh.auctionapp.repository.UserRepository;
+import com.atlantbh.auctionapp.request.RegisterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,19 @@ public class UserService {
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    public User register(RegisterRequest registerRequest){
+        if (userRepository.existsByEmail(registerRequest.getEmail())) {
+            throw new RuntimeException("Email is already in use");
+        }
+        User user = userRepository.save(new User(
+                registerRequest.getFirstName(),
+                registerRequest.getLastName(),
+                registerRequest.getEmail(),
+                passwordEncoder.encode(registerRequest.getPassword())
+        ));
+        user.setPassword(null);
+        return user;
     }
 }
