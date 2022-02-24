f9a8fac34211aa26187eb3a626323f97666b80da
MagicPojska <pojskicsafet@gmail.com>
2022-02-24 11:33:30 +0100

    Fixed not generating expiration date and added more helper methods to JWT

--- server/auctionapp/src/main/java/com/atlantbh/auctionapp/security/JsonWebToken.java
+++ server/auctionapp/src/main/java/com/atlantbh/auctionapp/security/JsonWebToken.java
@@ -1,6 +1,7 @@
 package com.atlantbh.auctionapp.security;
 
 import com.atlantbh.auctionapp.model.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,8 +11,16 @@ import java.util.Date;
 
 @Component
 public class JsonWebToken {
+
+    private static String appName;
     private static String jwtSecret;
     private static int jwtExpiration;
+    private static SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
+
+    @Value("${app.app}")
+    public void setAppName(String appName) {
+        JsonWebToken.appName = appName;
+    }
 
     @Value("${app.jwtSecret}")
     public void setJwtSecret(String jwtSecret) {
@@ -19,16 +28,34 @@ public class JsonWebToken {
     }
 
     @Value("${app.jwtExpiration}")
-    public static void setJwtExpiration(int jwtExpiration) {
+    public void setJwtExpiration(int jwtExpiration) {
         JsonWebToken.jwtExpiration = jwtExpiration;
+    }
+
+    private static Date generateExpirationDate() {
+        return new Date(new Date().getTime() + JsonWebToken.jwtExpiration * 1000);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        Claims claims;
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(jwtSecret)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (Exception e) {
+            claims = null;
+        }
+        return claims;
     }
 
     public static String generateJWTToken(User user) {
         long timestamp = System.currentTimeMillis();
         return Jwts.builder()
+                .setIssuer(appName)
                 .setIssuedAt(new Date(timestamp))
-                .setExpiration(new Date((new Date()).getTime() + jwtExpiration))
-                .signWith(SignatureAlgorithm.ES256.HS256, jwtSecret)
+                .setExpiration(generateExpirationDate())
+                .signWith(SIGNATURE_ALGORITHM, jwtSecret)
                 .claim("id", user.getId())
                 .compact();
     }
