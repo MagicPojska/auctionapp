1a2e3ceb313eb6d0514e690157a340c2fd997289
MagicPojska <pojskicsafet@gmail.com>
2022-02-26 21:23:03 +0100

    Edited login route in controller so it works with spring security

--- server/auctionapp/src/main/java/com/atlantbh/auctionapp/controllers/UserController.java
+++ server/auctionapp/src/main/java/com/atlantbh/auctionapp/controllers/UserController.java
@@ -7,10 +7,17 @@ import com.atlantbh.auctionapp.request.RegisterRequest;
 import com.atlantbh.auctionapp.response.LoginResponse;
 import com.atlantbh.auctionapp.response.LogoutResponse;
 import com.atlantbh.auctionapp.response.RegisterResponse;
-import com.atlantbh.auctionapp.security.JsonWebToken;
+import com.atlantbh.auctionapp.security.JwtUtil;
+import com.atlantbh.auctionapp.security.JwtUserDetailsService;
 import com.atlantbh.auctionapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,28 +28,53 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/auth")
 public class UserController {
-    private final UserService userService;
+    @Autowired
+    private JwtUserDetailsService userServiceDetails;
 
     @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
 
     @PostMapping("/register")
     public ResponseEntity<RegisterResponse> register(@RequestBody @Valid RegisterRequest registerRequest) {
         User user = userService.register(registerRequest);
-        return ResponseEntity.ok(new RegisterResponse(user, JsonWebToken.generateJWTToken(user)));
+        return ResponseEntity.ok(new RegisterResponse(user, JwtUtil.generateJWTToken(user)));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
-        User user = userService.login(loginRequest);
-        return ResponseEntity.ok(new LoginResponse(user, JsonWebToken.generateJWTToken(user)));
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest) throws Exception{
+        try {
+            authenticate(loginRequest.getEmail(), loginRequest.getPassword());
+
+            final UserDetails userDetails = userServiceDetails.loadUserByUsername(loginRequest.getEmail());
+
+            User user = userService.login(loginRequest);
+            return ResponseEntity.ok(new LoginResponse(user, jwtUtil.generateToken(userDetails)));
+        } catch (AuthenticationException authExc) {
+            throw new RuntimeException("Invalid Login Credentials");
+        }
     }
+
 
     @PostMapping("/logout")
     public ResponseEntity<LogoutResponse> logout(@RequestBody @Valid LogoutRequest logoutRequest) {
         String message = userService.logout(logoutRequest);
         return ResponseEntity.ok(new LogoutResponse(message));
+    }
+
+    private void authenticate(String email, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
     }
 }
