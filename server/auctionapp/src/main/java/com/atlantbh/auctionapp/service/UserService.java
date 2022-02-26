67082daacb24b43e0451cd8e80e0d6ac708e0dce
MagicPojska <pojskicsafet@gmail.com>
2022-02-26 21:17:30 +0100

    Edited UserService class so it works with new configuration

--- server/auctionapp/src/main/java/com/atlantbh/auctionapp/service/UserService.java
+++ server/auctionapp/src/main/java/com/atlantbh/auctionapp/service/UserService.java
@@ -5,15 +5,19 @@ import com.atlantbh.auctionapp.repository.UserRepository;
 import com.atlantbh.auctionapp.request.LoginRequest;
 import com.atlantbh.auctionapp.request.LogoutRequest;
 import com.atlantbh.auctionapp.request.RegisterRequest;
-import com.atlantbh.auctionapp.security.JsonWebToken;
+import com.atlantbh.auctionapp.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService  {
+    @Autowired
     private final UserRepository userRepository;
+    @Autowired
     private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtUtil jsonWebToken;
 
     @Autowired
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
@@ -37,16 +41,13 @@ public class UserService {
 
     public User login(LoginRequest loginRequest) {
         User user = userRepository.findByEmail(loginRequest.getEmail());
-        if (user == null || !passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
-            throw new RuntimeException("The email address or password you entered is not valid");
-        }
         user.setPassword(null);
         return user;
     }
 
     public String logout(LogoutRequest logoutRequest){
         String token = logoutRequest.getToken();
-        JsonWebToken.invalidateToken(token);
+        jsonWebToken.invalidateToken(token);
         return "User has been logged out!";
     }
 }
