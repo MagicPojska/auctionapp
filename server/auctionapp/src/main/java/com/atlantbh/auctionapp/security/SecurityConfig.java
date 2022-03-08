00568790f5a4c46d6ff68a02a7c7d8185d8a01f3
MagicPojska <pojskicsafet@gmail.com>
2022-03-08 14:52:47 +0100

    Permited all requests on product/**

--- server/auctionapp/src/main/java/com/atlantbh/auctionapp/security/SecurityConfig.java
+++ server/auctionapp/src/main/java/com/atlantbh/auctionapp/security/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/auth/login", "/auth/register").permitAll()
                 .antMatchers(AUTH_WHITELIST).permitAll()
-                .antMatchers("/product/items").permitAll()
+                .antMatchers("/product/**").permitAll()
                 .anyRequest().authenticated().and()
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
