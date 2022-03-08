3f84d5a85d7ca63c823b21ce1d0ef1edfb1e6b43
MagicPojska <pojskicsafet@gmail.com>
2022-03-08 14:04:27 +0100

    Implemented ProductService and method for getting paginated and sorted products

---
+++ server/auctionapp/src/main/java/com/atlantbh/auctionapp/service/ProductService.java
@@ -0,0 +1,36 @@
+package com.atlantbh.auctionapp.service;
+
+import com.atlantbh.auctionapp.model.ProductEntity;
+import com.atlantbh.auctionapp.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ProductEntity> getAllProducts(Integer pageNumber, Integer pageSize, String sortBy){
+        Sort sortOrder;
+        if(sortBy.equals("startDate")){
+            sortOrder = Sort.by(sortBy).descending();
+        }else{
+            sortOrder = Sort.by(sortBy);
+        }
+        Pageable paging = PageRequest.of(pageNumber, pageSize, sortOrder);
+
+        Page<ProductEntity> pagedResult = productRepository.findAll(paging);
+        if(pagedResult.hasContent()) {
+            return pagedResult.getContent();
+        } else {
+            return new ArrayList<ProductEntity>();
+        }
+    }
+}
