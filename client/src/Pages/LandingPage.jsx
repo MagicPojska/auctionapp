6dbc58da8ada14477c0e6a57c59cc05515d33701
MagicPojska <pojskicsafet@gmail.com>
2022-03-10 15:33:26 +0100

    Added new components for infinite scroll items, categories and highlighted product

--- client/src/Pages/LandingPage.jsx
+++ client/src/Pages/LandingPage.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { Link, useLocation } from "react-router-dom";
 import { getCategoriesList, getProductsSorted } from "../utilities/api";
-import { BsChevronRight } from "react-icons/bs";
-import { categoriesPath, shopProductPath } from "../utilities/paths";
+import { shopProductPath } from "../utilities/paths";
+import LandingPageCategories from "../components/LandingPageCategories";
+import HighlightedProduct from "../components/HighlightedProduct";
+import LandingPageItems from "../components/LandingPageItems";
 
 const LandingPage = () => {
   const [products, setProducts] = useState([]);
@@ -66,64 +68,9 @@ const LandingPage = () => {
   return (
     <>
       <div className="px-40 2xl:px-72 pt-4 h-[39rem] bg-bgWhite flex justify-between">
-        <div className="h-full">
-          <h3 className="text-base leading-5 font-bold ml-4 mb-4 text-purple">
-            CATEGORIES
-          </h3>
-          <div className="h-[32rem] w-64 overflow-y-scroll scrollbar">
-            {categories.map(
-              (item) =>
-                item.supercategoryId === null && (
-                  <div
-                    className="bg-white h-14  flex p-4 border-b-[1px] border-gray-300 items-center"
-                    key={item.id}
-                  >
-                    <Link
-                      to={`${categoriesPath}/${item.categoryName.toLowerCase()}`}
-                      className="text-base leading-6 font-normal"
-                    >
-                      {item.categoryName}
-                    </Link>
-                  </div>
-                )
-            )}
-          </div>
-          <div className="bg-white h-14 w-64 flex p-4 border-b-[1px] border-gray-300 items-center">
-            <Link
-              to={categoriesPath}
-              className="text-base leading-6 font-normal"
-            >
-              All Categories
-            </Link>
-          </div>
-        </div>
+        <LandingPageCategories categories={categories} />
 
-        <div className="my-24 w-[46rem] 2xl:w-[50rem] flex ml-5">
-          <div className="flex flex-col mr-3">
-            <h2 className="text-3xl leading-10 font-normal mb-4">
-              {highlightedProduct.productName}
-            </h2>
-            <span className="text-3xl leading-10 font-normal text-purple mb-4">
-              Start From ${parseFloat(highlightedProduct.startPrice).toFixed(2)}
-            </span>
-            <p className="text-base leading-6 font-normal text-textTetriary">
-              {highlightedProduct.description}
-            </p>
-            <Link
-              to={`${shopProductPath}/${highlightedProduct.id}`}
-              className="mt-auto flex border-4 border-purple w-48 h-14 justify-center items-center leading-7 text-base font-bold"
-            >
-              BID NOW &nbsp;&nbsp;{" "}
-              <BsChevronRight className="stroke-2 text-xs" />
-            </Link>
-          </div>
-
-          <img
-            src={highlightedProduct.images}
-            alt=""
-            className="h-[26rem] w-[22rem] object-cover"
-          />
-        </div>
+        <HighlightedProduct highlightedProduct={highlightedProduct} />
       </div>
 
       <div className="mx-40 2xl:mx-72 mt-16">
@@ -152,36 +99,11 @@ const LandingPage = () => {
           </div>
         </div>
 
-        <InfiniteScroll
-          dataLength={products.length} //This is important field to render the next data
-          next={fetchNextPage}
+        <LandingPageItems
+          products={products}
           hasMore={hasMore}
-        >
-          <div className="grid grid-cols-4 gap-x-4 gap-y-8 mt-8">
-            {products.map((item) => (
-              <Link
-                to={`${shopProductPath}/${item.id}`}
-                className="flex flex-col"
-                key={item.id}
-              >
-                <img
-                  src={item.images.split(",")[0]}
-                  className="object-cover aspect-square"
-                  alt=""
-                />
-                <h4 className="mt-3 text-2xl leading-6 font-semibold">
-                  {item.productName}
-                </h4>
-                <p className="text-l leading-6 font-normal text-textTetriary">
-                  Start From&nbsp;
-                  <span className="text-purple font-bold">
-                    ${parseFloat(item.startPrice).toFixed(2)}
-                  </span>
-                </p>
-              </Link>
-            ))}
-          </div>
-        </InfiniteScroll>
+          fetchNextPage={fetchNextPage}
+        />
       </div>
     </>
   );
