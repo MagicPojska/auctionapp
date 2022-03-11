9b89c1e50b73670a1cdc106c6be6da1c57476351
MagicPojska <pojskicsafet@gmail.com>
2022-03-11 11:58:49 +0100

    Made functional filter page that shows products from superategory clicked on landing page

--- client/src/Pages/FilterPage.jsx
+++ client/src/Pages/FilterPage.jsx
@@ -1,47 +1,27 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import { getCategoriesList, getProductsByCategory } from "../utilities/api";
 import { shopProductPath } from "../utilities/paths";
 
 const FilterPage = () => {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [superCategoryList, setSuperCategoryList] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
   const [pageNumber, setPageNumber] = useState(1);
-  const { id } = useParams();
 
+  const { id } = useParams();
+  const location = useLocation();
   let superCategories;
 
-  const pageSize = 9;
+  const PAGE_SIZE = 9;
 
   useEffect(() => {
     (async () => {
       await getCategories();
-      await getProductsCategory();
+      await getProducts(0);
     })();
   }, []);
-
-  const getProductsCategory = async () => {
-    try {
-      //This returns supercategory from list of key value paires from superCategories object literal with subcategoryId and supercategoryId
-      const supercategoryIds = superCategories
-        .map((item) => {
-          if (item.supercategoryId == id) {
-            return item.subcategoryId;
-          }
-        })
-        .filter((item) => item !== undefined);
-
-      const response = await getProductsByCategory(
-        0,
-        pageSize,
-        supercategoryIds
-      );
-
-      setProducts(response.data.content);
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   const getCategories = async () => {
     try {
@@ -60,14 +40,61 @@ const FilterPage = () => {
           return;
         })
         .filter((item) => item !== undefined);
+
+      setSuperCategoryList(superCategories);
     } catch (error) {
       console.log(error);
     }
   };
 
+  const getProducts = async (page) => {
+    try {
+      let supercategoryIds;
+      if (page === 0) {
+        supercategoryIds = getCategoryPairs(superCategories);
+      } else {
+        supercategoryIds = getCategoryPairs(superCategoryList);
+      }
+      const response = await getProductsByCategory(
+        page,
+        PAGE_SIZE,
+        supercategoryIds
+      );
+      if (page === 0) {
+        setHasMore(true);
+        setProducts([]);
+        setProducts(response.data.content);
+      } else {
+        setProducts([...products, ...response.data.content]);
+      }
+
+      if (response.data.last) {
+        setHasMore(false);
+      }
+      setPageNumber(page);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const loadMoreProducts = async () => {
+    await getProducts(pageNumber + 1);
+  };
+
+  const getCategoryPairs = (categoryList) => {
+    //This returns supercategory from list of key value paires from superCategories object literal with subcategoryId and supercategoryId
+    return categoryList
+      .map((item) => {
+        if (item.supercategoryId == id) {
+          return item.subcategoryId;
+        }
+      })
+      .filter((item) => item !== undefined);
+  };
+
   return (
     <div className="mx-40 2xl:mx-72 flex justify-between">
-      <div className="w-64 min-w-max border-2 p-6">
+      <div className="w-64 min-w-max h-max border-2 p-6">
         <h3 className="text-base font-bold text-purple">PRODUCT CATEGORIES</h3>
         {categories.map(
           (item) =>
@@ -80,6 +107,7 @@ const FilterPage = () => {
             )
         )}
       </div>
+
       <div className="ml-6 ">
         <div className="grid grid-cols-3 gap-x-4 gap-y-8">
           {products.map((item) => (
@@ -104,6 +132,16 @@ const FilterPage = () => {
               </p>
             </Link>
           ))}
+        </div>
+        <div className="w-full flex justify-center mt-20">
+          <button
+            className={`py-4 px-14 bg-purple text-white text-base font-bold leading-7 ${
+              hasMore ? "" : "hidden"
+            }`}
+            onClick={loadMoreProducts}
+          >
+            Explore More
+          </button>
         </div>
       </div>
     </div>
