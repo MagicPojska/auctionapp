b6a6a5fd5db1fe7d64c4724d90522f873e6ab84e
MagicPojska <pojskicsafet@gmail.com>
2022-03-13 22:46:58 +0100

    Added product name as title, starting price and a block with some info

--- client/src/Pages/ProductOverviewPage.jsx
+++ client/src/Pages/ProductOverviewPage.jsx
@@ -15,12 +15,35 @@ const ProductOverviewPage = () => {
   }, []);
 
   return (
-    <div>
+    <>
       <CurrentPageNav title={product.productName} />
-      <p>
-        {product.id} - {product.productName} - {product.startDate}
-      </p>
-    </div>
+      <div className="mx-40 2xl:mx-72 flex">
+        <div className="w-1/2">images</div>
+        <div>
+          <h1 className="w-1/2 text-3xl font-normal mb-4">
+            {product.productName}
+          </h1>
+          <p className="text-base font-light mb-4 text-purple">
+            Starts from <span className="font-bold">${product.startPrice}</span>
+          </p>
+          <div className="text-base font-light py-6 px-9 border-2 space-y-4">
+            <p>
+              Highest bid:{" "}
+              <span className="font-bold text-purple">
+                ${product.startPrice}
+              </span>
+            </p>
+            <p>
+              Number of bids: <span className="font-bold text-purple">0</span>
+            </p>
+            <p>
+              Time left:{" "}
+              <span className="font-bold text-purple">${product.endDate}</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
