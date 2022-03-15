228d425761ded3fab3e370aeb2ee8199df4e9b86
MagicPojska <pojskicsafet@gmail.com>
2022-03-15 14:19:14 +0100

    Added new aspect ratio to a photo

--- client/src/components/ImageSelection.jsx
+++ client/src/components/ImageSelection.jsx
@@ -2,7 +2,7 @@ const ImageSelection = ({ image, images, setImage }) => {
   return (
     <div className="w-1/2">
       <div className="mr-28 flex flex-col">
-        <img src={image} className="object-cover aspect-[7/9]" alt="" />
+        <img src={image} className="object-cover aspect-[8/10]" alt="" />
         <div
           className={`h-24 w-full flex ${
             images.length === 5 ? "justify-between" : "space-x-10"
