20af081ca79eb64670533f0ec8b9e85fb131a712
MagicPojska <pojskicsafet@gmail.com>
2022-03-17 09:31:14 +0100

    Redesigned 404 page

--- client/src/Pages/NotFoundPage.jsx
+++ client/src/Pages/NotFoundPage.jsx
@@ -1,31 +1,27 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import { homePath } from "../utilities/paths";
 
 const NotFoundPage = () => {
   return (
-    <div
-      className="
-    flex items-center justify-center w-screen h-screen"
-    >
-      <div className="px-40 py-20 bg-white rounded-md shadow-xl">
-        <div className="flex flex-col items-center">
-          <h1 className="font-bold text-purple text-9xl">404</h1>
+    <div className="flex items-center justify-center mx-40">
+      <div className="flex flex-col items-center">
+        <img
+          src="/images/auction-app-logo.png"
+          alt="logo"
+          className="mb-16 mt-36"
+        />
+        <h1 className="font-normal text-purple text-[192px] mb-16">404</h1>
 
-          <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
-            <span className="text-red-500">Oops!</span> Page not found
-          </h6>
+        <h6 className="mb-8 text-2xl font-normal text-center text-gray-800 md:text-3xl">
+          Ooops! Looks like the page is Not Found
+        </h6>
 
-          <p className="mb-8 text-center text-gray-500 md:text-lg">
-            The page you’re looking for doesn’t exist.
-          </p>
-
-          <Link
-            to="/"
-            className="px-6 py-2 text-sm font-semibold text-purple bg-blue-100"
-          >
-            Go home
-          </Link>
-        </div>
+        <Link
+          to={homePath}
+          className="flex border-4 border-purple w-40 h-14 justify-center items-center leading-7 text-base font-bold"
+        >
+          GO BACK
+        </Link>
       </div>
     </div>
   );
