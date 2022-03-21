efeec7805bf18b5f2cc4c6998c1893e2fd3cb43e
MagicPojska <pojskicsafet@gmail.com>
2022-03-21 08:32:55 +0100

    Removed unused import

--- server/auctionapp/src/main/java/com/atlantbh/auctionapp/service/ProductService.java
+++ server/auctionapp/src/main/java/com/atlantbh/auctionapp/service/ProductService.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 
