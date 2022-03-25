d3dae4f5cefca7860e9822325f540ab3efbf24cf
MagicPojska <pojskicsafet@gmail.com>
2022-03-25 11:37:04 +0100

    Put each enum value in new row

--- server/auctionapp/src/main/java/com/atlantbh/auctionapp/model/enums/SortBy.java
+++ server/auctionapp/src/main/java/com/atlantbh/auctionapp/model/enums/SortBy.java
@@ -1,7 +1,11 @@
 package com.atlantbh.auctionapp.model.enums;
 
 public enum SortBy {
-    PRODUCT_NAME("productName"), START_DATE("startDate"), END_DATE("endDate"), START_PRICE("startPrice"), HIGH_PRICE("highPrice") ;
+    PRODUCT_NAME("productName"),
+    START_DATE("startDate"),
+    END_DATE("endDate"),
+    START_PRICE("startPrice"),
+    HIGH_PRICE("highPrice") ;
 
 
     private String sort;
