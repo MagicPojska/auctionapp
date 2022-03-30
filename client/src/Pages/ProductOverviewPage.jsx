b6931576c8b6b442989e6b1c5c38404c33ffbc9a
MagicPojska <pojskicsafet@gmail.com>
2022-03-30 09:43:48 +0200

    Fixed error when no product with id exists to show a message

--- client/src/Pages/ProductOverviewPage.jsx
+++ client/src/Pages/ProductOverviewPage.jsx
@@ -74,98 +74,108 @@ const ProductOverviewPage = () => {
 
   return (
     <>
-      <CurrentPageNav title={product.productName} />
-
-      {notification && (
-        <Notification
-          notification={notification}
-          setNotification={setNotification}
-        />
-      )}
-
-      <div className="mx-40 mt-8 2xl:mx-72 flex">
-        <ImageSelection images={images} />
-
-        <div className="w-1/2">
-          <h1 className="text-3xl font-normal mb-4">{product.productName}</h1>
-          <p className="text-base font-light mb-4 text-purple">
-            Starts from <span className="font-bold">${product.startPrice}</span>
-          </p>
-          <div className="text-base font-light py-6 px-9 border-2 space-y-4 mb-16 w-fit">
-            <p>
-              Highest bid:{" "}
-              <span className="font-bold text-purple">
-                {product.highestBid !== null
-                  ? "$" + product.highestBid
-                  : "No bids"}
-              </span>
-            </p>
-            <p>
-              Number of bids:{" "}
-              <span className="font-bold text-purple">
-                {product.numberOfBids}
-              </span>
-            </p>
-            <p>
-              Time left:{" "}
-              <span className="font-bold text-purple">
-                {parseTimeLeft(timeLeft)}
-              </span>
-            </p>
-          </div>
-
-          {timeLeft.minutes < 0 ? (
-            <></>
-          ) : (
-            <div className="flex">
-              <input
-                type="text"
-                value={bid}
-                onChange={(e) => {
-                  if (e.target.value.match("^[0-9.]*$") != null) {
-                    setBid(e.target.value);
-                  }
-                }}
-                className="appearance-none border-2 py-3 px-8 mr-6 w-4/6 focus:outline-none"
-                placeholder={`Enter $${
-                  product.highestBid !== null
-                    ? product.highestBid
-                    : product.startPrice
-                } or higher`}
-                disabled={(!user || user.id === product.userId) && true}
-              />
-              <button
-                onClick={placeBid}
-                className={`mt-auto flex border-4 border-purple w-48 h-14 justify-center items-center leading-7 text-base font-bold ${
-                  (!user || user.id === product.userId) && "opacity-25"
-                }`}
-                disabled={(!user || user.id === product.userId) && true}
-              >
-                PLACE BID &nbsp;&nbsp;{" "}
-                <BsChevronRight className="stroke-2 text-xs" />
-              </button>
-            </div>
+      {product ? (
+        <>
+          <CurrentPageNav title={product.productName} />
+          {notification && (
+            <Notification
+              notification={notification}
+              setNotification={setNotification}
+            />
           )}
 
-          <div>
-            <div className="border-b-2 mt-16">
-              <div className="space-x-12 text-xl leading-6 font-normal text-purple">
-                <button
-                  className="pb-4  
+          <div className="mx-40 mt-8 2xl:mx-72 flex">
+            <ImageSelection images={images} />
+
+            <div className="w-1/2">
+              <h1 className="text-3xl font-normal mb-4">
+                {product.productName}
+              </h1>
+              <p className="text-base font-light mb-4 text-purple">
+                Starts from{" "}
+                <span className="font-bold">${product.startPrice}</span>
+              </p>
+              <div className="text-base font-light py-6 px-9 border-2 space-y-4 mb-16 w-fit">
+                <p>
+                  Highest bid:{" "}
+                  <span className="font-bold text-purple">
+                    {product.highestBid !== null
+                      ? "$" + product.highestBid
+                      : "No bids"}
+                  </span>
+                </p>
+                <p>
+                  Number of bids:{" "}
+                  <span className="font-bold text-purple">
+                    {product.numberOfBids}
+                  </span>
+                </p>
+                <p>
+                  Time left:{" "}
+                  <span className="font-bold text-purple">
+                    {parseTimeLeft(timeLeft)}
+                  </span>
+                </p>
+              </div>
+
+              {timeLeft.minutes < 0 ? (
+                <></>
+              ) : (
+                <div className="flex">
+                  <input
+                    type="text"
+                    value={bid}
+                    onChange={(e) => {
+                      if (e.target.value.match("^[0-9.]*$") != null) {
+                        setBid(e.target.value);
+                      }
+                    }}
+                    className="appearance-none border-2 py-3 px-8 mr-6 w-4/6 focus:outline-none"
+                    placeholder={`Enter $${
+                      product.highestBid !== null
+                        ? product.highestBid
+                        : product.startPrice
+                    } or higher`}
+                    disabled={(!user || user.id === product.userId) && true}
+                  />
+                  <button
+                    onClick={placeBid}
+                    className={`mt-auto flex border-4 border-purple w-48 h-14 justify-center items-center leading-7 text-base font-bold ${
+                      (!user || user.id === product.userId) && "opacity-25"
+                    }`}
+                    disabled={(!user || user.id === product.userId) && true}
+                  >
+                    PLACE BID &nbsp;&nbsp;{" "}
+                    <BsChevronRight className="stroke-2 text-xs" />
+                  </button>
+                </div>
+              )}
+
+              <div>
+                <div className="border-b-2 mt-16">
+                  <div className="space-x-12 text-xl leading-6 font-normal text-purple">
+                    <button
+                      className="pb-4  
                  border-b-[3px] border-purple px-6"
-                >
-                  Details
-                </button>
+                    >
+                      Details
+                    </button>
+                  </div>
+                </div>
+
+                <div className="mt-8 text-base leading-6 font-normal text-textTetriary">
+                  <span>Note:</span>
+                  <p>{product.description}</p>
+                </div>
               </div>
             </div>
-
-            <div className="mt-8 text-base leading-6 font-normal text-textTetriary">
-              <span>Note:</span>
-              <p>{product.description}</p>
-            </div>
           </div>
+        </>
+      ) : (
+        <div className="flex justify-center my-44 text-2xl font-bold">
+          Product with id:{id} not found
         </div>
-      </div>
+      )}
     </>
   );
 };
