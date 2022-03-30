6f7d4e8f5853ea12e32c37f3c608e746fcf9e36b
MagicPojska <pojskicsafet@gmail.com>
2022-03-30 15:08:39 +0200

    Created three dots that indicate steps in form for adding item

--- client/src/Pages/AddItemPage.jsx
+++ client/src/Pages/AddItemPage.jsx
@@ -35,6 +35,18 @@ const AddItemPage = () => {
     }));
   };
 
+  const renderPurpleDot = () => (
+    <div className="w-8 h-8 rounded-full shadow-xl flex justify-center items-center">
+      <div className="w-4 h-4 rounded-full bg-purple"></div>
+    </div>
+  );
+
+  const renderHollowDot = () => (
+    <div className="w-8 h-8 rounded-full shadow-xl flex justify-center items-center">
+      <div className="w-4 h-4 rounded-full border-2 border-purple"></div>
+    </div>
+  );
+
   const renderSwitch = () => {
     switch (step) {
       case 1:
@@ -70,6 +82,13 @@ const AddItemPage = () => {
   return (
     <>
       <CurrentPageNav title="Seller" />
+      <div className="flex w-full justify-center items-center">
+        {renderPurpleDot()}
+        <hr className="w-20" />
+        {step === 2 || step === 3 ? renderPurpleDot() : renderHollowDot()}
+        <hr className="w-20" />
+        {step === 3 ? renderPurpleDot() : renderHollowDot()}
+      </div>
       <div className="w-[46rem] mx-auto mt-12 justify-center">
         {renderSwitch()}
       </div>
