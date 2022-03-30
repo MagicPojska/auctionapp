8fb19ebc4f5ed77b248fc049e5dd22ac022319d2
MagicPojska <pojskicsafet@gmail.com>
2022-03-30 22:38:27 +0200

    Removed unused import

--- server/auctionapp/src/main/java/com/atlantbh/auctionapp/security/JwtFilter.java
+++ server/auctionapp/src/main/java/com/atlantbh/auctionapp/security/JwtFilter.java
@@ -2,7 +2,6 @@ package com.atlantbh.auctionapp.security;
 
 import com.atlantbh.auctionapp.service.UserService;
 import io.jsonwebtoken.ExpiredJwtException;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
