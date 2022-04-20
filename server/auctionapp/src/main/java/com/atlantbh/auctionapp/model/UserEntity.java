d01a1f78be0a8e3abb7705af2e1e3972e9fc9309
MagicPojska <pojskicsafet@gmail.com>
2022-04-20 13:45:11 +0200

    Removed unused imports

--- server/auctionapp/src/main/java/com/atlantbh/auctionapp/model/UserEntity.java
+++ server/auctionapp/src/main/java/com/atlantbh/auctionapp/model/UserEntity.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
